ccc93910a8ba213e1bf38fea3d5f3c74db640c7f
mc12019 <159210084+mc12019@users.noreply.github.com>
2024-08-28 11:57:44 +0800

    Framework Update

---
+++ debugmode/CommandMapping.h
@@ -0,0 +1,70 @@
+/*
+ WIP still, 8.28
+ Maybe this will be done in the winter vacation.
+ "Damn I'd go back to school,"
+ "Oh my github and those accounts will be inactive!"
+*/
+#ifndef H_COMMANDMAPPING
+#define H_COMMANDMAPPING
+#include <stdlib.h>
+struct cm_CommandMapping {
+	char *command;
+	int id;
+};
+struct cm_MapsIndex {
+	int count;
+	int nextIndex;
+};
+static int _cm_firstAvailable = 0;
+static int _cm_index = 0;
+static struct cm_MapsIndex **_cm_mapsIndexs;
+static struct cm_CommandMapping **_cm_mapsArrays;
+
+/* Explanation
+ To allocate memory dynamically, I use a point point.
+ Because a point nearly equals a array in fact.
+ Points always occupy 8 bytes so I just need a index,
+ And every point the point point points points to an unfixed length's heap
+ memory.
+ */
+void cm_allocate(int count) {  // NOLINT
+	_cm_mapsIndexs[_cm_index] = (struct cm_MapsIndex*) malloc(
+			sizeof(struct cm_MapsIndex));
+	_cm_mapsIndexs[_cm_index]->count = count;
+	if (!_cm_index)
+		_cm_mapsIndexs[_cm_index - 1]->nextIndex = _cm_index;
+	_cm_mapsIndexs[_cm_index]->nextIndex = -1;
+	_cm_mapsArrays[_cm_index] = (struct cm_CommandMapping*) malloc(
+			sizeof(struct cm_CommandMapping) * count);
+	++_cm_index;
+}
+// For debug usage
+void cm_heapDelete(int index) {  // NOLINT
+	if (index < _cm_firstAvailable)
+		return;
+	if (index == _cm_firstAvailable)
+		_cm_firstAvailable = _cm_mapsIndexs[_cm_firstAvailable]->nextIndex;
+	else
+		_cm_mapsIndexs[index - 1]->nextIndex = index + 1;
+	free(_cm_mapsArrays[index]);
+	free(_cm_mapsIndexs[index]);
+}
+// countIndex means which one instead of mapsIndex
+struct cm_CommandMapping cm_get(int countIndex) {  // NOLINT
+	int currentIndex = _cm_firstAvailable, total = 0, lastTotal = 0;
+	while (currentIndex != -1) {
+		total += _cm_mapsIndexs[currentIndex]->count;
+		if (countIndex < total)
+			return *(_cm_mapsArrays[total - lastTotal]);
+		lastTotal = total;
+		currentIndex = _cm_mapsIndexs[currentIndex]->nextIndex;
+	}
+	struct cm_CommandMapping tmp;
+	tmp.id = -1;
+}
+// GC for debug
+void _cm_autoclean() {
+	
+}
+
+#endif
--- debugmode/Interpreter.h
+++ debugmode/Interpreter.h
@@ -2,39 +2,47 @@
 #define F_INTERPRETER
 #define LNG 20
 #define ARGS 10
-char com[LNG];
-char args[ARGS][LNG];
-void interpret(const char* s) {
+#include <stdio.h>
+char _in_com[LNG]; // NOLINT
+char _in_args[ARGS][LNG]; // NOLINT
+void in_interpret(const char* s) { // NOLINT
     // argi -> argument index segi -> segement index
     int inQuote = 0, argi = -1, segi = 0;
     for (int r = 0; r != ARGS; ++r) {
         for (int i = 0; i != LNG; ++i) {
-            com[i] = '\0';
-            args[r][i] = '\0';
+            _in_com[i] = '\0';
+            _in_args[r][i] = '\0';
         }
     }
     for (int i = 0; s[i] != '\0'; ++i) {
+        if (s[i] == '\n')
+            continue;
         if (s[i] == '\"' || s[i] == '\'') {
             inQuote = !inQuote;
             continue;
         }
-        if (argi < 0) {
-            com[segi++] = s[i];
-            continue;
-        }
         if (s[i] == ' ' && !inQuote) {
             if (argi < 0)
-                com[segi] = '\0';
+                _in_com[segi] = '\0';
             else
-                args[argi][segi] = '\0';
+                _in_args[argi][segi] = '\0';
             segi = 0;
             ++argi;
             continue;
         }
-        args[argi][segi++] = s[i];
+        if (argi < 0) {
+            _in_com[segi++] = s[i];
+            continue;
+        }
+        _in_args[argi][segi++] = s[i];
     }
-    if (segi != 0) com[segi] = '\0';
+    if (segi != 0) {
+        if (argi < 0)
+            _in_com[segi] = '\0';
+        else
+            _in_args[argi][segi] = '\0';
+    }
 }
-char* getCom() { return com; }
-char* getArg(int i) { return args[i]; }
+char* in_getCom() { return _in_com; } // NOLINT
+char* in_getArg(int r) { return _in_args[r]; } // NOLINT
 #endif
